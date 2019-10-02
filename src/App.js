11f43321f2d3578ba224c4117c3dcb35c71b4a05
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-10-01 20:17:25 -0400

    add about page

--- src/App.js
+++ src/App.js
@@ -1,124 +1,17 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
+import AboutPage from "./components/AboutPage";
 import "./App.css";
+import "./Landing.css";
+import "./About.css";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
       <Navbar />
-      <p className="content">
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum
-      </p>
+      <AboutPage />
     </div>
   );
 }
---
+++ src/components/AboutPage.js
@@ -0,0 +1,137 @@
+import React from "react";
+import clean from "../clean.svg";
+import performant from "../performant.svg";
+import responsive from "../responsive.svg";
+import lightbulb from "../lightbulb.svg";
+
+const AboutPage = () => {
+  return (
+    <div className="about-background">
+      <div className="about-container">
+        {/* title */}
+        <h1 className="about" data-aos="fade-down">
+          About
+        </h1>
+        {/* title underline */}
+        <div className="underline" data-aos="flip-right"></div>
+        {/* bio */}
+        <p className="content">
+          <span className="accent">Hi</span>, my name is Dylan Baker and I'm a
+          <span className="accent"> full stack web developer</span> with a
+          passion for <span className="accent">blockchain</span> technology. I
+          take pride in creating clean, intuitive, responsive and performant web
+          apps.
+        </p>
+        <div className="icon-container">
+          {/* clean icon + text */}
+          <div
+            className="icon-subcontainer"
+            data-aos="fade-right"
+            data-aos-anchor-placement="top-center"
+          >
+            <img src={clean} alt="clean icon" />
+            <h2 className="subheader-accent">Clean</h2>
+            <p className="content">
+              I strive to keep things simple and elegant, not only design
+              elements but my codebase and development process as well.
+            </p>
+          </div>
+          {/* lightbulb icon + text */}
+          <div
+            className="icon-subcontainer"
+            data-aos="fade-left"
+            data-aos-anchor-placement="top-center"
+          >
+            <img src={lightbulb} alt="lightbulb icon" />
+            <h2 className="subheader-accent">Intuitive</h2>
+            <p className="content">
+              Focused on producing easy to use interfaces and user experiences
+              that feel natural.
+            </p>
+          </div>
+          {/* devices icon + text */}
+          <div
+            className="icon-subcontainer"
+            data-aos="fade-right"
+            data-aos-anchor-placement="top-center"
+          >
+            <img src={responsive} alt="devices icon" />
+            <h2 className="subheader-accent">Responsive</h2>
+            <p className="content">
+              With the amount of web traffic from mobile devices constantly
+              increasing, ensuring designs look and feel good on screens of all
+              sizes is a top priority.
+            </p>
+          </div>
+          {/* lightning icon + text */}
+          <div
+            className="icon-subcontainer"
+            data-aos="fade-left"
+            data-aos-anchor-placement="top-center"
+          >
+            <img src={performant} alt="lightning bolt icon" />
+            <h2 className="subheader-accent">Fast</h2>
+            <p className="content">
+              Utilizing best practices and the latest technologies to minimize
+              load times and provide an expereince that feels smooth.
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Quam id leo in vitae
+        turpis. Vulputate odio ut enim blandit volutpat maecenas volutpat
+        blandit. Nisl vel pretium lectus quam id. Erat velit scelerisque in
+        dictum non. Aliquet nibh praesent tristique magna sit amet. Luctus
+        accumsan tortor posuere ac ut consequat semper. Enim blandit volutpat
+        maecenas volutpat. Vivamus arcu felis bibendum ut tristique et egestas.
+        Adipiscing commodo elit at imperdiet dui accumsan sit. Bibendum est
+        ultricies integer quis. Est ultricies integer quis auctor elit sed
+        vulputate. Eu feugiat pretium nibh ipsum consequat nisl. Tristique et
+        egestas quis ipsum suspendisse ultrices gravida dictum fusce. Duis
+        ultricies lacus sed turpis. Adipiscing enim eu turpis egestas pretium
+        aenean. Venenatis a condimentum vitae sapien pellentesque habitant morbi
+        tristique senectus. Sit amet mattis vulputate enim nulla. Est
+        ullamcorper eget nulla facilisi etiam dignissim diam. Ac felis donec et
+        odio pellentesque diam volutpat commodo sed. Arcu risus quis varius quam
+        quisque. Ac turpis egestas sed tempus urna et. Leo in vitae turpis
+        massa. Cras ornare arcu dui vivamus arcu felis bibendum ut. Erat velit
+        scelerisque in dictum non consectetur a. Natoque penatibus et magnis dis
+        parturient montes nascetur. Pellentesque diam volutpat commodo sed
+        egestas egestas fringilla. Ultricies integer quis auctor elit sed
+        vulputate. In nisl nisi scelerisque eu. Eget nunc lobortis mattis
+        aliquam faucibus purus. Risus quis varius quam quisque id. Habitant
+        morbi tristique senectus et netus et malesuada fames. At tempor commodo
+        ullamcorper a lacus vestibulum sed arcu. Et ligula ullamcorper malesuada
+        proin libero nunc consequat interdum. Sed odio morbi quis commodo odio
+        aenean sed. Nulla pellentesque dignissim enim sit amet. Tristique
+        senectus et netus et malesuada fames ac. In mollis nunc sed id semper
+        risus in hendrerit. Vitae tortor condimentum lacinia quis. Ultricies mi
+        quis hendrerit dolor magna eget est. Id interdum velit laoreet id.
+        Malesuada pellentesque elit eget gravida cum sociis natoque penatibus
+        et. Libero id faucibus nisl tincidunt. Aliquam vestibulum morbi blandit
+        cursus risus at ultrices mi tempus. Ac turpis egestas sed tempus. Porta
+        nibh venenatis cras sed felis. Dolor sed viverra ipsum nunc. Non blandit
+        massa enim nec dui nunc. Auctor urna nunc id cursus. Quis viverra nibh
+        cras pulvinar mattis nunc sed blandit libero. Amet consectetur
+        adipiscing elit pellentesque habitant morbi tristique senectus. Massa
+        eget egestas purus viverra accumsan. Tincidunt dui ut ornare lectus sit
+        amet. Quis lectus nulla at volutpat diam ut venenatis. Mauris ultrices
+        eros in cursus turpis massa. Eu facilisis sed odio morbi quis commodo
+        odio aenean sed. Odio aenean sed adipiscing diam donec. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt.
+        Vitae et leo duis ut diam. Lobortis elementum nibh tellus molestie.
+        Convallis a cras semper auctor neque vitae. Donec et odio pellentesque
+        diam volutpat commodo sed. Nibh praesent tristique magna sit amet purus
+        gravida. In fermentum et sollicitudin ac orci phasellus egestas tellus.
+        Euismod elementum nisi quis eleifend quam adipiscing. Enim sit amet
+        venenatis urna. Scelerisque viverra mauris in aliquam sem fringilla ut
+        morbi. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim.
+        Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
