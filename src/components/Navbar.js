a608a075a63a757b02c33916b0cc4e7d725cacef
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-10-03 15:43:09 -0400

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,44 +8,42 @@ const Navbar = () => {
       data-aos="fade-down"
       data-aos-anchor=".content"
       data-aos-anchor-placement="top-top"
-      className="nav-container"
+      className="navbar"
     >
-      <nav className="navbar">
-        <a href="/#landing-page" style={{ height: "30px" }}>
-          <img
-            border="0"
-            alt="White logo"
-            src={whiteLogo}
-            width="30"
-            height="30"
-          />
-        </a>
-        <div id="nav-filler" />
-        <a
-          id="about"
-          href="#about-container"
-          data-aos="about-highlight"
-          data-aos-anchor="#about-container"
-        >
-          About
-        </a>
-        <a
-          id="projects"
-          href="#projects"
-          data-aos="projects-highlight"
-          data-aos-anchor=".projects-container"
-        >
-          Projects
-        </a>
-        <a
-          id="contact"
-          href="#contact"
-          data-aos="contact-highlight"
-          data-aos-anchor=".contact-container"
-        >
-          Contact
-        </a>
-      </nav>
+      <a href="/#landing-page" style={{ height: "30px" }}>
+        <img
+          border="0"
+          alt="White logo"
+          src={whiteLogo}
+          width="30"
+          height="30"
+        />
+      </a>
+      <div id="nav-filler" />
+      <a
+        id="about"
+        href="#about-container"
+        data-aos="about-highlight"
+        data-aos-anchor="#about-container"
+      >
+        About
+      </a>
+      <a
+        id="projects"
+        href="#projects"
+        data-aos="projects-highlight"
+        data-aos-anchor=".projects-container"
+      >
+        Projects
+      </a>
+      <a
+        id="contact"
+        href="#contact"
+        data-aos="contact-highlight"
+        data-aos-anchor=".contact-container"
+      >
+        Contact
+      </a>
       <ScrollIndicator />
     </div>
   );
