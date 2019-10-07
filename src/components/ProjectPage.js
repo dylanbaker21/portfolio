dd0f6c84c420ab46ff9e4680768c27ea4196811d
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-10-07 01:41:45 -0400

    add hashify to project section

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ProjectTile from "./ProjectTile";
 import vscoImg from "../images/vsco-viewer-screenshot.png";
+import hashifyImg from "../images/hashify-screenshot.png";
 class ProjectPage extends Component {
   state = {};
   render() {
@@ -13,6 +14,13 @@ class ProjectPage extends Component {
         <div className="underline" data-aos="flip-right"></div>
         <div id="projects-container">
           <ProjectTile
+            title="Hashify"
+            header="Web application utilizing smart contracts to submit hashes to the Ethereum blockchain for verification and permanence"
+            thumbnail={hashifyImg}
+            webLink="https://hashify.tools"
+            gitLink="https://github.com/dylanbaker21/hashify"
+          />
+          <ProjectTile
             title="VSCO Viewer"
             header="Web scraping tool used to display full-resolution social media
       profile pictures"
@@ -20,25 +28,6 @@ class ProjectPage extends Component {
             webLink="https://www.vscoviewer.com"
             gitLink="https://github.com/dylanbaker21/pfp-viewer"
           />
-          <div className="project-tile">
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-          </div>
-          <div className="project-tile">
-            <p>Lorem Ipsum</p>
-
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-          </div>
-          <div className="project-tile">
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-            <p>Lorem Ipsum</p>
-          </div>
         </div>
       </div>
     );
